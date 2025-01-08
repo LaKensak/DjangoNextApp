a5f25593cd41f6b2f5ffa119962f67856fb6b9bb
LaKensak <150548098+LaKensak@users.noreply.github.com>
2025-01-08 14:46:48 +0100

    Deploy

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -64,9 +64,10 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:3001",
     "http://127.0.0.1:3000",
+    "django-next-app-napjo.app"
 ]
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost:3000","http://127.0.0.1:3000","http://localhost:3001"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost:3000","http://127.0.0.1:3000","http://localhost:3001","django-next-app-napjo.app"]
 
 CORS_ALLOW_METHODS = [
     'DELETE',
